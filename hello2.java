1226c1006994db88a13fd642ed27575cbf10dcb7
Abhilash LN <abhilashz3138@gmail.com>
2024-07-05 10:31:55 +0530

    goldrate and java changed

--- hello2.java
+++ hello2.java
@@ -3,6 +3,10 @@ public class hello2{
 public static void  main(String args[]){
 System.out.println("hello world from gedit");
 System.out.println("just checking inside");
+
+for(int i=0;i<=10;i++){
+System.out.println(i);
+}
 }
 
 
